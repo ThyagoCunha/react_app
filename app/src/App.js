27e26160a41ba8db8d95713309c3fdd1d81d9c9f
Thyago Cunha <thyago_cunha@MacBook-Air-de-Thyago.local>
2019-08-28 22:55:46 -0300

    Criando o componente para usar o css

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import SqueareCool from './components/SquareCool/index.js'
 
+// function App() {
+//   return (
+//     <h1>Hello World</h1>
+//   );
+// }
 
-function App() {
-  return (
-    <h1>Hello World</h1>
-  );
-}
+const App = ()=> <SqueareCool />
 
 export default App;
---
+++ app/src/components/SquareCool/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const SquareCool = () => (
+  <button>X</button>
+);
+
+export default SquareCool;
+
+//Outras opçao-----------------------------
+
+// import React, { Component } from "react";
+
+// class SquareCool extends Component {
+//   render(){
+//     return <button>X x</button>
+//   }
+// }
+
+// export default SquareCool;
